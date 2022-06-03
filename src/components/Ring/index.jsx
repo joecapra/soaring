39c207f2eb6ac33ae62410867e3ef2274b854341
Joe Capra <jcapra@160over90.com>
2022-06-02 21:23:34 -0700

    test

--- src/components/Ring/index.jsx
+++ src/components/Ring/index.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
 import "./styles.scss";
 
 export default function Ring() {
@@ -7,10 +9,14 @@ export default function Ring() {
   return (
     <div className="ring">
       RING={params.name}
-      <img
-        src={process.env.PUBLIC_URL + "/images/Crystal-Bishop-30-1.jpg"}
-        alt="zdfg"
-      />
+      <TransformWrapper>
+        <TransformComponent>
+          <img
+            src={process.env.PUBLIC_URL + "/images/Crystal-Bishop-30-1.jpg"}
+            alt="zdfg"
+          />
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
 }
